06b5366d4678620278ac74ceed1f2a5161759f16
hpunk <gustavoalzamora1997@gmail.com>
2022-06-14 09:58:20 -0300

    test coverage up to 100%

--- test_app.py
+++ test_app.py
@@ -21,7 +21,7 @@ def test_hello_world():
 #when using kmedoids algorithm for clustering, then receive the same amount of groups as the k parameter sent in the request
 def test_clustering_kmedoids():
     cluster_number = 3
-    response = client.get('/clustering?st=15&pr=1501&di=150103&sd=2019-01-01&ed=2019-01-15&alg=0&k='+str(cluster_number)+'&eps=0&mins=0')
+    response = client.get('/clustering?st=15&pr=1501&di=150103&sd=2017-01-01&ed=2017-01-31&alg=0&k='+str(cluster_number)+'&eps=0&mins=0')
     assert response.status_code == 200
     assert len(response.json()['response']) == cluster_number
 
@@ -30,9 +30,9 @@ def test_clustering_dbscan():
     response = client.get('/clustering?st=15&pr=1501&di=150103&sd=2019-01-01&ed=2019-02-02&alg=1&eps=0.20&mins=5&k=0')
     assert response.status_code == 200
     assert len(response.json()['response']) == 3
-    assert response.json()['response'][2]['group'] == '-1'
-    assert response.json()['response'][1]['group'] == '1'
-    assert response.json()['response'][0]['group'] == '0'
+    assert response.json()['response'][2]['group'] in ['Grupo 0','Grupo 1','Sin grupo']
+    assert response.json()['response'][1]['group'] in ['Grupo 0','Grupo 1','Sin grupo']
+    assert response.json()['response'][0]['group'] in ['Grupo 0','Grupo 1','Sin grupo']
 
 #when using hierarchical approach to generate dendrogram, then only receive 1 as confirmation that the request has been succesfully handled
 def test_clustering_hierarchical_dendrogram():
@@ -43,17 +43,23 @@ def test_clustering_hierarchical_dendrogram():
 #when using hierarchical approach to generate scatter plot and send data, then the number of groups is the same as the one used in the request
 def test_clustering_hierarchical_scatter():
     number_clusters = 3
-    response = client.get('/clustering/hierarchical-to-scatter?st=15&pr=1501&di=150103&sd=2019-01-01&ed=2019-01-15&alg=2&k='+str(number_clusters))
+    response = client.get('/clustering/hierarchical-to-scatter?st=15&pr=1501&di=150103&sd=2018-01-01&ed=2018-01-15&alg=2&k='+str(number_clusters))
     assert response.status_code == 200
     assert len(response.json()['response']) == number_clusters
 
 
 #when number of cases to be clustered is greater than the max number of cases stated for a good performance, then a sampling is made
 def test_sampling_of_clustering():
-    count_response = client.get('/clustering/count?st=15&pr=1501&di=150103&sd=2019-01-01&ed=2019-03-30')
-    data_response = client.get('/clustering?st=15&pr=1501&di=150103&sd=2019-01-01&ed=2019-03-30&alg=0&k=3&mins=0&eps=0')
+    count_response = client.get('/clustering/count?st=15&pr=1501&di=150103&sd=2020-01-01&ed=2020-03-30')
+    data_response = client.get('/clustering?st=15&pr=1501&di=150103&sd=2020-01-01&ed=2020-03-30&alg=0&k=3&mins=0&eps=0')
 
-    assert count_response.json()['count'] == 699
+    assert count_response.json()['count'] == 1020
+    assert len(data_response.json()['response']) == 3
+    data_response_json = data_response.json()
+    assert len(data_response_json['response'][0]['data']) + len(data_response_json['response'][1]['data']) + len(data_response_json['response'][2]['data']) == max_number_cases
+
+    data_response = client.get('/clustering/hierarchical-to-scatter?st=15&pr=1501&di=150103&sd=2020-01-01&ed=2020-03-30&alg=2&k=3')
+
     assert len(data_response.json()['response']) == 3
     data_response_json = data_response.json()
     assert len(data_response_json['response'][0]['data']) + len(data_response_json['response'][1]['data']) + len(data_response_json['response'][2]['data']) == max_number_cases
--- test_myutils.py
+++ test_myutils.py
@@ -1,4 +1,38 @@
-from myutils import distance_function
+from myutils import distance_function, generate_query_for_count, generate_query_for_clustering
+
+#testing the generation of query for clustering with different parameters
+def test_query_generation_for_clustering():
+    only_dates_query = generate_query_for_clustering("2020-01-01","2020-02-02",0,0,0)
+    assert "state" not in only_dates_query
+    assert "province" not in only_dates_query
+    assert "district" not in only_dates_query
+
+    only_dates_and_state_query = generate_query_for_clustering("2020-01-01","2020-02-02",1,0,0)
+    assert "state" in only_dates_and_state_query
+    assert "province" not in only_dates_and_state_query
+    assert "district" not in only_dates_and_state_query
+
+    only_dates_and_province_and_state_query = generate_query_for_clustering("2020-01-01","2020-02-02",1,101,0)
+    assert "state" not in only_dates_and_province_and_state_query
+    assert "province" in only_dates_and_province_and_state_query
+    assert "district" not in only_dates_and_province_and_state_query
+
+#testing the generation of query for count with different parameters
+def test_query_generation_for_count():
+    only_dates_query = generate_query_for_count("2020-01-01","2020-02-02",0,0,0)
+    assert "state" not in only_dates_query
+    assert "province" not in only_dates_query
+    assert "district" not in only_dates_query
+
+    only_dates_and_state_query = generate_query_for_count("2020-01-01","2020-02-02",1,0,0)
+    assert "state" in only_dates_and_state_query
+    assert "province" not in only_dates_and_state_query
+    assert "district" not in only_dates_and_state_query
+
+    only_dates_and_province_and_state_query = generate_query_for_count("2020-01-01","2020-02-02",1,101,0)
+    assert "state" not in only_dates_and_province_and_state_query
+    assert "province" in only_dates_and_province_and_state_query
+    assert "district" not in only_dates_and_province_and_state_query
 
 #testing maximum distance value of two completely different objects, based on if columns were categorical (only different values) or if were numerical (extreme values)
 def test_maximum_distance_from_distance_function():
